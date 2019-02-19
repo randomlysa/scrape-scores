ec0995645815be58f8be8b25e59f325d3320fb55
randomlysa <su@randomlysa.com>
2019-02-19 00:29:28 -0600

    remove cache from server - it's strictly a dev server now

--- src/server.js
+++ src/server.js
@@ -11,35 +11,47 @@ const express = require('express');
 const app = express();
 app.use(cors());
 
+const getData = require('./server-getData');
+
 // your express configuration here
 
+// Disabling cache since this is now only a local dev server.
 // Store data in cache for 24 hours (86400 seconds). After that, delete, and
 // it will be re-fetched.
-const myCache = new NodeCache({ stdTTL: 86400, checkperiod: 120 });
+// const myCache = new NodeCache({ stdTTL: 86400, checkperiod: 120 });
 
 app.get('/', (request, response) => {
   response.send('Hello -- this is the wrong page!');
 });
 
 app.get('/macs', async (request, response) => {
-  const cacheData = myCache.get('macs_data');
-  if (cacheData) {
-    console.log('has cache');
-    return response.send(cacheData);
-  } else {
-    console.log('no can has cache');
-  }
+  // const cacheData = myCache.get('macs_data');
+  // if (cacheData) {
+  //   console.log('has cache');
+  //   return response.send(cacheData);
+  // } else {
+  //   console.log('no can has cache');
+  // }
 
   fetch('http://www.mnchristianschools.org/athletics.html')
     .then(res => res.text())
     .then(body => {
       // Save 'body' to cache.
-      myCache.set('macs_data', body, (err, success) => {
-        if (err) console.log('Error: ', err);
-        if (success) console.log('Success: ', success);
-      });
+      // myCache.set('macs_data', body, (err, success) => {
+      //   if (err) console.log('Error: ', err);
+      //   if (success) console.log('Success: ', success);
+      // });
       console.log('in fetch');
-      response.send(body);
+      const waitData = async rData => {
+        return await getData.getData(rData);
+      };
+
+      waitData(body).then(d => {
+        Promise.all([d[0].boys, d[1].girls]).then(final => {
+          // final[0] = boys, final[1] = girls
+          response.send(final);
+        });
+      });
     });
 });
 
