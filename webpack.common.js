244d8aa0a71bae1d49b09538eed03feeb0e8634f
randomlysa <su@randomlysa.com>
2019-02-18 20:18:00 -0600

    move bundle analyzer to production

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,6 @@
 const path = require('path');
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const CleanWebpackPlugin = require('clean-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-  .BundleAnalyzerPlugin;
 
 module.exports = {
   entry: {
@@ -40,6 +38,5 @@ module.exports = {
     filename: '[name].bundle.js',
     chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [new BundleAnalyzerPlugin()]
+  }
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,15 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
-    })
+    }),
+    new BundleAnalyzerPlugin()
   ]
 });
